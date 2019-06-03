ff36fb0eca717462172fa9bc4235219822b46ae9
Linya Hu <lh62594@yahoo.com>
2019-06-03 10:10:12 -0400

    deploy production

---
+++ build/precache-manifest.33efa1b6ef9275727ed964296aab9a39.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5549333d760ad6ad0e1",
+    "url": "./static/css/main.5d10d6d5.chunk.css"
+  },
+  {
+    "revision": "c5549333d760ad6ad0e1",
+    "url": "./static/js/main.cd65b18e.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "1adfb98e7af36f7729f4",
+    "url": "./static/js/2.a35d333f.chunk.js"
+  },
+  {
+    "revision": "f7272f2764ad585b8dbe57e2045dec08",
+    "url": "./static/media/logo.f7272f27.png"
+  },
+  {
+    "revision": "e839959b13422b5af829188345495c19",
+    "url": "./static/media/landing-plain.e839959b.png"
+  },
+  {
+    "revision": "61577c0297a46301d1b768bec784a6dc",
+    "url": "./static/media/landing-login.61577c02.png"
+  },
+  {
+    "revision": "876a3d8861cb927d15d34a018d97f093",
+    "url": "./static/media/landing.876a3d88.png"
+  },
+  {
+    "revision": "920e6ab93eee928221296773ecab60c6",
+    "url": "./index.html"
+  }
+];
